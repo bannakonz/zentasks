13292925fdc290297a76ea3ff2e36b65fce5bfbe
bannakonz <bannakon10@gmail.com>
2025-06-27 17:00:56 +0700

    feat: add log, and comment when using field dependency injection

--- src/main/java/com/bannakon/zentasks/controller/TodoController.java
+++ src/main/java/com/bannakon/zentasks/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.bannakon.zentasks.controller;
 
 import com.bannakon.zentasks.entity.Todo;
 import com.bannakon.zentasks.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/api/todos")
 @RestController
 public class TodoController {
 
+    //    @Autowired // ถ้าใช้ field DI, ไม่ใส่ @Autowired, จะ error 500 NullPointerException, because "this.todoService" is null
     private final TodoService todoService;
 
     public TodoController(TodoService todoService) {
@@ -21,6 +24,7 @@ public class TodoController {
 
     @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos() {
+        log.info("TodoController getAllDataTodos: {}", todoService.getAllDataTodos());
         return ResponseEntity.ok(todoService.getAllDataTodos());
     }
 }
--- src/main/java/com/bannakon/zentasks/service/TodoService.java
+++ src/main/java/com/bannakon/zentasks/service/TodoService.java
@@ -3,11 +3,15 @@ package com.bannakon.zentasks.service;
 import com.bannakon.zentasks.entity.Todo;
 import com.bannakon.zentasks.repository.TodoRepository;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class TodoService {
+
+//    @Autowired // ถ้าใช้ field DI, ไม่ใส่ @Autowired, จะ error 500 NullPointerException, because "this.todoRepository" is null
     private final TodoRepository todoRepository;
 
     public TodoService(TodoRepository todoRepository) {
@@ -15,6 +19,7 @@ public class TodoService {
     }
 
     public List<Todo> getAllDataTodos() {
+        log.info("TodoService todoRepository findAll: {}", todoRepository.findAll());
         return todoRepository.findAll();
     }
 }
