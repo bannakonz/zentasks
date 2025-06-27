84358f2bae0f3eb47aa8480d7c7b467410fb6d61
bannakonz <bannakon10@gmail.com>
2025-06-27 14:07:59 +0700

    feat: add controller, entiry, db.sql to init data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO todos (title, completed) VALUES ('Buy groceries', false);
+INSERT INTO todos (title, completed) VALUES ('Study Spring Boot', false);
+INSERT INTO todos (title, completed) VALUES ('Complete project', true);
