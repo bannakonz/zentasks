3a32fc3011f8210931866cef2d5dbb2b052299f4
Bannakon P <bannakon@Bannakons-MacBook-Pro.local>
2025-06-27 10:35:38 +0700

    first commit

---
+++ src/main/java/com/bannakon/zentasks/ZentasksApplication.java
@@ -0,0 +1,13 @@
+package com.bannakon.zentasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bannakon/zentasks/ZentasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bannakon.zentasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
