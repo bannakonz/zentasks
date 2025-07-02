a65ce2657abe26a7902e583565507f9e54427f18
bannakonz <bannakon10@gmail.com>
2025-07-02 10:09:07 +0700

    test: add unit test controller

--- src/test/java/com/bannakon/zentasks/controller/TodoControllerTest.java
+++ src/test/java/com/bannakon/zentasks/controller/TodoControllerTest.java
@@ -1,19 +1,21 @@
 package com.bannakon.zentasks.controller;
 
+import com.bannakon.zentasks.dto.TodoRequest;
+import com.bannakon.zentasks.dto.UpdateTodoRequest;
 import com.bannakon.zentasks.entity.Todo;
 import com.bannakon.zentasks.service.TodoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +32,9 @@ class TodoControllerTest {
     @MockitoBean
     private TodoService todoService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     void shouldReturnAllTodos() throws Exception {
@@ -41,5 +46,45 @@ class TodoControllerTest {
                 .andExpect(jsonPath("$[0].title").value("Task"));
     }
 
+    @Test
+    void shouldCreateTodo() throws Exception {
+        TodoRequest request = new TodoRequest();
+        request.setTitle("New Task");
+        Todo created = new Todo(1L, "New Task", false);
+
+        when(todoService.createDataTodo(request)).thenReturn(created);
+
+        mockMvc.perform(post("/api/todos")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.title").value("New Task"));
+    }
+
+    @Test
+    void shouldUpdateTodo() throws Exception {
+        UpdateTodoRequest request = new UpdateTodoRequest();
+        request.setTitle("Update Task");
+        request.setCompleted(true);
+
+        Todo updated = new Todo(1L, "Update Task", true);
+
+        when(todoService.updateTodo(1L, request)).thenReturn(updated);
+
+        mockMvc.perform(put("/api/todos/1")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Update Task"))
+                .andExpect(jsonPath("$.completed").value(true));
+    }
+
+    @Test
+    void shouldDeleteTodo() throws Exception {
+
+        mockMvc.perform(delete("/api/todos/1")
+                        .contentType("application/json"))
+                .andExpect(status().isNoContent()); // 204
+    }
 
 }
